8f9a37b02489ff6da50900d496d700abb70ae2a1
David Sapp <david@mobius.digital>
2020-12-15 23:00:40 -0700

    remove blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Gatsby + Netlify CMS Starter - No blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This repo contains an custom implementation of Netlify\'s Gatsby + Netlify CMS Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ exports.createPages = ({ actions, graphql }) => {
               slug
             }
             frontmatter {
-              tags
+              path
               templateKey
             }
           }
@@ -29,43 +29,33 @@ exports.createPages = ({ actions, graphql }) => {
       return Promise.reject(result.errors)
     }
 
-    const posts = result.data.allMarkdownRemark.edges
+    // Filter out the footer, navbar, and meetups so we don't create pages for those
+    const postOrPage = result.data.allMarkdownRemark.edges.filter(edge => {
+      if (edge.node.frontmatter.templateKey === "navbar") {
+        return false;
+      } else if (edge.node.frontmatter.templateKey === "footer") {
+        return false;
+      } else {
+        return !Boolean(edge.node.fields.slug.match(/^\/meetups\/.*$/));
+      }
+    });
 
-    posts.forEach((edge) => {
-      const id = edge.node.id
+    postOrPage.forEach(edge => {
+      let component, pathName;
+      if (edge.node.frontmatter.templateKey === "home-page") {
+        pathName = "/";
+        component = path.resolve(`src/pages/index.js`);
+      } else {
+        pathName = edge.node.frontmatter.path || edge.node.fields.slug;
+        component = path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`);
+      }
+      const id = edge.node.id;
       createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
+        path: pathName,
+        component,
         // additional data can be passed via context
         context: {
           id,
-        },
-      })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach((edge) => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach((tag) => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
         },
       })
     })
